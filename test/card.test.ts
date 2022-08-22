9f1e05d1540903a660f2e2dd0f56ed6f668241e2
Hannah Ellis <hannah.ellis@ten10.com>
2022-08-22 12:16:33 +0100

    Testing the suit of Wands

--- test/card.test.ts
+++ test/card.test.ts
@@ -1,14 +1,33 @@
-class Card{
-    constructor(id:number){
+type Suit = "Trump" | "Wands";
+class Card {
+  private suit_: Suit;
+  constructor(public id: number) {
+    if (id < 22) {
+      this.suit_ = "Trump";
+    } else {
+      this.suit_ = "Wands";
     }
-    suit(){
-        return "Trump";
-    }
+  }
+  suit(): Suit {
+    return this.suit_;
+  }
 }
 
-describe("Card",()=>{
-    test("a card should have a suit",()=>{
-        const card = new Card(0);
+describe("Card", () => {
+  new Array(22)
+    .fill(0)
+    .map((_, index) => new Card(index))
+    .forEach((card) => {
+      test(`a card with id ${card.id} should have the suit of Trumps`, () => {
         expect(card.suit()).toBe("Trump");
-    })
-})
+      });
+    });
+  new Array(13)
+    .fill(0)
+    .map((_, index) => new Card(index + 22))
+    .forEach((card) => {
+      test(`a card with id ${card.id} should have the suit of Wands`, () => {
+        expect(card.suit()).toBe("Wands");
+      });
+    });
+});
