2271ef7911d9c3698a85611a12d113bd09fb3007
Hannah Ellis <hannah.ellis@ten10.com>
2022-08-22 16:15:51 +0100

    precalculating pip at construction for cards

--- src/model/Card.ts
+++ src/model/Card.ts
@@ -80,19 +80,22 @@ const pips: Pip[] = [
 
 export default class Card {
   private suit_: Suit;
+  private pip_: Pip;
   constructor(public id: number) {
     const value = Math.floor((id - 22) / 14);
     const suitsIndex = value < 0 ? 0 : value + 1;
     this.suit_ = suits[suitsIndex];
+    if (this.id < 22) {
+      this.pip_ = pips[this.id];
+    } else {
+      const pipIndex = ((this.id - 22) % 14) + 22;
+      this.pip_ = pips[pipIndex];
+    }
   }
   suit(): Suit {
     return this.suit_;
   }
   pip(): Pip {
-    if (this.id < 22) {
-      return pips[this.id];
-    }
-    const pipIndex = ((this.id - 22) % 14) + 22;
-    return pips[pipIndex];
+    return this.pip_;
   }
 }
