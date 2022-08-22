397aed5576d84d35cdc4f1d7bed32460de97e206
Hannah Ellis <hannah.ellis@ten10.com>
2022-08-22 12:24:34 +0100

    Testing all suits

--- test/card.test.ts
+++ test/card.test.ts
@@ -1,14 +1,17 @@
-type Suit = "Trump" | "Wands" | "Cups";
+type Suit = "Trump" | "Wands" | "Cups" | "Swords" | "Penticles";
 class Card {
   private suit_: Suit;
   constructor(public id: number) {
     if (id < 22) {
       this.suit_ = "Trump";
-    } else if(id < 22 +14) {
+    } else if (id < 22 + 14) {
       this.suit_ = "Wands";
-    }
-    else{
-        this.suit_ = "Cups";
+    } else if (id < 22 + 2 * 14) {
+      this.suit_ = "Cups";
+    } else if (id < 22 + 3 * 14) {
+      this.suit_ = "Swords";
+    } else {
+      this.suit_ = "Penticles";
     }
   }
   suit(): Suit {
@@ -39,6 +42,22 @@ describe("Card", () => {
     .forEach((card) => {
       test(`a card with id ${card.id} should have the suit of Cups`, () => {
         expect(card.suit()).toBe("Cups");
+      });
+    });
+  new Array(14)
+    .fill(0)
+    .map((_, index) => new Card(index + 22 + 2 * 14))
+    .forEach((card) => {
+      test(`a card with id ${card.id} should have the suit of Swords`, () => {
+        expect(card.suit()).toBe("Swords");
+      });
+    });
+  new Array(14)
+    .fill(0)
+    .map((_, index) => new Card(index + 22 + 3 * 14))
+    .forEach((card) => {
+      test(`a card with id ${card.id} should have the suit of Penticles`, () => {
+        expect(card.suit()).toBe("Penticles");
       });
     });
 });
