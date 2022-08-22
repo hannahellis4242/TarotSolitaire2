009597271f749de40a2d7a6cfe14ad69190eceb2
Hannah Ellis <hannah.ellis@ten10.com>
2022-08-22 12:35:56 +0100

    adding an extra describe block to the test

--- test/card.test.ts
+++ test/card.test.ts
@@ -13,44 +13,46 @@ class Card {
 }
 
 describe("Card", () => {
-  new Array(22)
-    .fill(0)
-    .map((_, index) => new Card(index))
-    .forEach((card) => {
-      test(`a card with id ${card.id} should have the suit of Trumps`, () => {
-        expect(card.suit()).toBe("Trump");
+  describe("Card.suit()", () => {
+    new Array(22)
+      .fill(0)
+      .map((_, index) => new Card(index))
+      .forEach((card) => {
+        test(`a card with id ${card.id} should have the suit of Trumps`, () => {
+          expect(card.suit()).toBe("Trump");
+        });
       });
-    });
-  new Array(14)
-    .fill(0)
-    .map((_, index) => new Card(index + 22))
-    .forEach((card) => {
-      test(`a card with id ${card.id} should have the suit of Wands`, () => {
-        expect(card.suit()).toBe("Wands");
+    new Array(14)
+      .fill(0)
+      .map((_, index) => new Card(index + 22))
+      .forEach((card) => {
+        test(`a card with id ${card.id} should have the suit of Wands`, () => {
+          expect(card.suit()).toBe("Wands");
+        });
       });
-    });
-  new Array(14)
-    .fill(0)
-    .map((_, index) => new Card(index + 22 + 14))
-    .forEach((card) => {
-      test(`a card with id ${card.id} should have the suit of Cups`, () => {
-        expect(card.suit()).toBe("Cups");
+    new Array(14)
+      .fill(0)
+      .map((_, index) => new Card(index + 22 + 14))
+      .forEach((card) => {
+        test(`a card with id ${card.id} should have the suit of Cups`, () => {
+          expect(card.suit()).toBe("Cups");
+        });
       });
-    });
-  new Array(14)
-    .fill(0)
-    .map((_, index) => new Card(index + 22 + 2 * 14))
-    .forEach((card) => {
-      test(`a card with id ${card.id} should have the suit of Swords`, () => {
-        expect(card.suit()).toBe("Swords");
+    new Array(14)
+      .fill(0)
+      .map((_, index) => new Card(index + 22 + 2 * 14))
+      .forEach((card) => {
+        test(`a card with id ${card.id} should have the suit of Swords`, () => {
+          expect(card.suit()).toBe("Swords");
+        });
       });
-    });
-  new Array(14)
-    .fill(0)
-    .map((_, index) => new Card(index + 22 + 3 * 14))
-    .forEach((card) => {
-      test(`a card with id ${card.id} should have the suit of Penticles`, () => {
-        expect(card.suit()).toBe("Penticles");
+    new Array(14)
+      .fill(0)
+      .map((_, index) => new Card(index + 22 + 3 * 14))
+      .forEach((card) => {
+        test(`a card with id ${card.id} should have the suit of Penticles`, () => {
+          expect(card.suit()).toBe("Penticles");
+        });
       });
-    });
+  });
 });
