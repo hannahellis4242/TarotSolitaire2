9951bf7cd98124f8e2533378085d6516626a651c
Hannah Ellis <hannah.ellis@ten10.com>
2022-08-23 11:02:02 +0100

    pulling out useful connstants to share across other tests

---
+++ test/utils.ts
@@ -0,0 +1,19 @@
+import Card from "../src/model/Card";
+
+export const majors = new Array(22).fill(0).map((_, i) => new Card(i));
+export const wands = new Array(14).fill(0).map((_, i) => new Card(i + 22));
+export const cups = new Array(14).fill(0).map((_, i) => new Card(i + 22 + 14));
+export const swords = new Array(14)
+  .fill(0)
+  .map((_, i) => new Card(i + 22 + 2 * 14));
+export const pentacles = new Array(14)
+  .fill(0)
+  .map((_, i) => new Card(i + 22 + 3 * 14));
+export const all = majors
+  .concat(wands)
+  .concat(cups)
+  .concat(swords)
+  .concat(pentacles);
+export const allPairs = all.flatMap((parent) =>
+  all.map((child) => [parent, child])
+);
