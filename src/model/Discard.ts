7e9faf6d00761e20d899edefb7730aadca0734bf
Hannah Ellis <hannah.ellis@ten10.com>
2022-08-23 10:52:00 +0100

    pulling all the code into separate files

---
+++ src/model/Discard.ts
@@ -0,0 +1,15 @@
+import Card from "./Card";
+import Layout from "./Layout";
+
+export default class Discard implements Location {
+  name: string;
+  cards: Card[];
+  constructor(public layout: Layout) {
+    this.name = "discard";
+    this.cards = layout.discard;
+  }
+  proposedChild(child: Card): boolean {
+    //discard accepts any child card
+    return true;
+  }
+}
