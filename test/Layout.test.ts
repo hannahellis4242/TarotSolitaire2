e4dca978cb9fe2c6fabc203c92ab816ea5801142
Hannah Ellis <hannah.ellis@ten10.com>
2022-08-23 10:04:37 +0100

    Adding a describe block to help group tests together again in layout tests

--- test/Layout.test.ts
+++ test/Layout.test.ts
@@ -27,23 +27,25 @@ const populate = (deck: Card[]): Layout => {
 };
 
 describe("Layout", () => {
-  test("the layout should have a tableau that consists of nine slots for cards", () => {
-    const layout = new Layout();
-    expect(layout.tableau.length).toBe(9);
-    expect(layout.tableau[0]).not.toBe(layout.tableau[1]); //make sure that each array of cards is actually a different array of cards
-  });
-  test("the layout should have a foundation of 5 slots for cards", () => {
-    const layout = new Layout();
-    expect(layout.foundation.length).toBe(5);
-    expect(layout.foundation[0]).not.toBe(layout.foundation[1]); //make sure that each array of cards is actually a different array of cards
-  });
-  test("the layout should have a pile of cards that starts off empty", () => {
-    const layout = new Layout();
-    expect(layout.pile.length).toBe(0);
-  });
-  test("the layout should have a discard pile of cards that starts off empty", () => {
-    const layout = new Layout();
-    expect(layout.discard.length).toBe(0);
+  describe("things the layout should have", () => {
+    test("the layout should have a tableau that consists of nine slots for cards", () => {
+      const layout = new Layout();
+      expect(layout.tableau.length).toBe(9);
+      expect(layout.tableau[0]).not.toBe(layout.tableau[1]); //make sure that each array of cards is actually a different array of cards
+    });
+    test("the layout should have a foundation of 5 slots for cards", () => {
+      const layout = new Layout();
+      expect(layout.foundation.length).toBe(5);
+      expect(layout.foundation[0]).not.toBe(layout.foundation[1]); //make sure that each array of cards is actually a different array of cards
+    });
+    test("the layout should have a pile of cards that starts off empty", () => {
+      const layout = new Layout();
+      expect(layout.pile.length).toBe(0);
+    });
+    test("the layout should have a discard pile of cards that starts off empty", () => {
+      const layout = new Layout();
+      expect(layout.discard.length).toBe(0);
+    });
   });
   describe("can create a layout with cards", () => {
     const deck = new Array(78).fill(0).map((_, i) => new Card(i));
