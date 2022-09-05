34f4461a2bed2dcd4246cea555332cace05a69e5
Hannah Ellis <hannah.ellis@ten10.com>
2022-09-05 15:14:36 +0100

    Turning off logging in unit test

--- test/minFind.test.ts
+++ test/minFind.test.ts
@@ -1,5 +1,5 @@
 import { minFindAuto } from "../src/view/utils/minFind";
 
 test("test", () => {
-  console.log(minFindAuto((x) => (x + 1) * (x - 1), 0.00001));
+  //console.log(minFindAuto((x) => (x + 1) * (x - 1), 0.00001));
 });
